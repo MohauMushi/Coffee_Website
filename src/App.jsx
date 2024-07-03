76bc577b34d67519bca26ec3d954a6c4092df175
Mohau Mushi <david1mushi1@gmail.com>
2024-07-03 10:43:08 +0200

    Added React and react-router-dom imports, and wrapped App component with BrowserRouter and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './Components/Layout/Layout'
 import './App.css'
 
 function App() {
@@ -6,6 +8,12 @@ function App() {
 
   return (
     <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout/>}>          
+        </Route>
+      </Routes>
+    </BrowserRouter>
       
     </>
   )
