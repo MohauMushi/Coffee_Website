cd750fc2131fc783198175058f99652491e70013
Mohau Mushi <david1mushi1@gmail.com>
2024-07-03 10:41:42 +0200

    Imported Navbar into the Layout component

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
 
 const Layout = () => {
   return (
     <div>
       <main>
+        <Navbar />
         <Outlet />
       </main>
     </div>
