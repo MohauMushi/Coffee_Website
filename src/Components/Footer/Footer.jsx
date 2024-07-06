ca6940bb1034fecbd79b41204e804cf779bbe598
Mohau Mushi <david1mushi1@gmail.com>
2024-07-06 19:09:23 +0200

    Updated Footer component with social media icons and styles

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import LogoImage from "/coffee.png";
+import FacebookIcon from "../../assets/icon-facebook.png"
+import InstagramIcon from "../../assets/icon-instagram.png"
+import TwitterIcon from "../../assets/icon-twitter.png"
 import "./Footer.css";
 
 const Footer = () => {
@@ -25,13 +28,13 @@ const Footer = () => {
           <span className="footer-follow-text">Follow us on</span>
           <div className="footer-follow-icons">
             <a href="https://www.facebook.com/">
-              <img src="" />
+              <img src={FacebookIcon} />
             </a>
             <a href="https://www.instagram.com/">
-              <img src="" />
+              <img src={InstagramIcon} />
             </a>
             <a href="https://www.twitter.com/">
-              <img src="" />
+              <img src={TwitterIcon} />
             </a>
           </div>
         </div>
